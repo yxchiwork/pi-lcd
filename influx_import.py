4f7b12beeb645f866d681ae6042fd00221a9e063
yxchi <yxchi0704@gmail.com>
2022-02-16 11:14:11 -0600

    update code for packages

--- influx_import.py
+++ influx_import.py
@@ -1,24 +1,26 @@
 from datetime import datetime
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
-import Adafruit_DHT
+import adafruit_dht
 import time
-import var.json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 24
 
-token = ""
-org = ""
+token = os.getenv('influx_token')
+org = os.getenv('influxdb_org')
 bucket = "Home-weather"
-influx_url=""
+influx_url=os.getenv('influx_url')
 
 while True:
 	with InfluxDBClient(url=influx_url, token=token, org=org) as client:
+#to update the adadruit read
 		humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 		write_api = client.write_api(write_options=SYNCHRONOUS)
 		if humidity is not None and temperature is not None:
-#		print(humidity, temperature)
 			data1 = "humidity,room=Bedroom humidity={}".format(humidity)
 			data2 = "temperature,room=Bedroom temperature={}".format(temperature)
 			write_api.write(bucket, org, data1)
